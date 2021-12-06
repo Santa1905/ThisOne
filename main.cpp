4381cfa098586b47fe3e6a8f159ee34651709dab
Ekaterina <kotuha443@gmail.com>
2021-12-06 20:37:53 +0200

    number1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+<<<<<<< HEAD
 using namespace std;
 int main()
+=======
+#include "funcs.h"
+
+using namespace std;
+
+int main()
+
+>>>>>>> ef299d07efd605275be9d8f9f01c65539abd0b56
 {
 	setlocale(LC_ALL, "Rus");
 	double a, b, c;
 	char z;
+<<<<<<< HEAD
 	double procent;
 
 	cout « "Pervoe chislo : ";
@@ -13,6 +23,20 @@ int main()
 	cin » b;
 	cout « "Deistvie: ";
 	cin » z;
+=======
+
+
+
+	cout « "Pervoe chislo : ";
+	cin » a;
+
+	cout « "Vtoroe: ";
+	cin » b;
+
+	cout « "Deistvie: ";
+	cin » z;
+
+>>>>>>> ef299d07efd605275be9d8f9f01c65539abd0b56
 	switch (z)
 	{
 	case '+':
@@ -27,6 +51,7 @@ int main()
 		cout « " * in progress";
 		break;
 
+<<<<<<< HEAD
 	case '/':
 		cout « " / in progress";
 		break;
@@ -35,6 +60,23 @@ int main()
 	default: cout « " ne to deistvie";;
 	}
 
+=======
+
+	case '/':
+		if (b == 0) {
+			cout « "Nelza" « endl;
+		}
+		else
+			cout « " / in progress";
+
+		break;
+
+
+	default: cout « " net deistviya";;
+
+
+	}
+>>>>>>> ef299d07efd605275be9d8f9f01c65539abd0b56
 	return 0;
 
 }
