ef299d07efd605275be9d8f9f01c65539abd0b56
Sentyabrina Sulakova <sulakova.sentyabrina@yandex.ru>
2021-12-06 21:05:57 +0300

    added funcs.cpp, funcs.h, modified main.cpp

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "funcs.h"
+
+using namespace std;
+
+int main()
+
+{
+	setlocale(LC_ALL, "Rus");
+	double a, b, c;
+	char z;
+
+
+
+	cout « "Pervoe chislo : ";
+	cin » a;
+
+	cout « "Vtoroe: ";
+	cin » b;
+
+	cout « "Deistvie: ";
+	cin » z;
+
+	switch (z)
+	{
+	case '+':
+		cout « " + in progress";
+		break;
+
+	case '-':
+		cout « " - in progress";
+		break;
+
+	case "*":
+		cout « " * in progress";
+		break;
+
+
+	case '/':
+		if (b == 0) {
+			cout « "Nelza" « endl;
+		}
+		else
+			cout « " / in progress";
+
+		break;
+
+
+	default: cout « " net deistviya";;
+
+
+	}
+	return 0;
+
+}
